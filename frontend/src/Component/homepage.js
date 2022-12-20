3381bf9b69ce93297a3b0a6c32fb84b199e8a730
Karthik-184 <patelkarthikreddy@gmail.com>
2022-12-20 18:38:43 +0530

    frontend

--- frontend/src/Component/homepage.js
+++ frontend/src/Component/homepage.js
@@ -1,11 +1,33 @@
 import React ,{ useEffect, useState} from "react";
+import * as xlsx  from "xlsx";
+
+const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = xlsx.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = xlsx.utils.sheet_to_json(worksheet);
+            console.log(json);
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }
+}
 
 function HomePage (){
     return(
-        <div>
-            Hello World
-        </div>
-
+        <form>
+            <label htmlFor="upload">Upload File</label>
+            <input
+                type="file"
+                name="upload"
+                id="upload"
+                onChange={readUploadFile}
+            />
+        </form>
     )
 }
 
